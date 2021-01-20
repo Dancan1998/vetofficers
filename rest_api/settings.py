82fd1fe5960cbce916c7143a36392b30298532a4
Dancan Chibole <dancankingstar@gmail.com>
2021-01-20 15:56:25 -0800

    removed static in my django settings

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:8000",
 #     "http://localhost:3000",
