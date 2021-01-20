14c334af9badec4aa06112749ff04bfe11802f2e
Dancan Chibole <dancankingstar@gmail.com>
2021-01-20 15:59:43 -0800

    changed back the sercret key

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '7z(= % qjl=o52 & +3s4v & @23ok2le8mx*2r11e7tasu %) = f!r29+'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = config('DEBUG', default=False, cast=bool)
